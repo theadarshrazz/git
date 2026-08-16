39e9bdf8f065a3d559261310f5591dae4f4992f5
theadarshrazz <adarshraj1223@gmail.com>
2026-08-16 17:18:30 +0000

    Initial Commit

--- python.py
+++ python.py
@@ -1,3 +1,26 @@
-x=int(input("Enter The First Number :- "))
-y=int(input("Enter The Second number :- "))
-print((x+y)/2)
+n = int(input("Enter The Number :- "))
+
+bus = list(map(int, input().split()))
+
+bus.sort()
+
+result = []
+i = 0
+
+while i < n:
+    start = i
+
+    while i + 1 < n and bus[i + 1] == bus[i] + 1:
+        i += 1
+
+    end = i
+
+    if end - start >= 2:
+        result.append(f"{bus[start]}-{bus[end]}")
+    else:
+        for j in range(start, end + 1):
+            result.append(str(bus[j]))
+
+    i += 1
+
+print(" ".join(result))
